3bbf66898a05155e4fecc240719d8d23801c9b78
wangyujiewj <wyjkroos@qq.com>
2018-04-04 17:01:49 +0800

    add a file

---
+++ TTSonline/app/src/main/java/com/example/acer/ttsonline/MainActivity.java
@@ -0,0 +1,516 @@
+package com.example.acer.ttsonline;
+
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+import android.content.BroadcastReceiver;
+import java.io.IOException;
+import android.app.Activity;
+import android.content.Context;
+import android.content.IntentFilter;
+import android.view.Menu;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import java.io.OutputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import android.widget.ArrayAdapter;
+import com.iflytek.cloud.RecognizerResult;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+import android.bluetooth.*;
+import android.util.Log; //打印信息
+
+import com.iflytek.cloud.SpeechConstant;
+import com.iflytek.cloud.SpeechError;
+import android.widget.ListView;
+import android.content.Intent;
+
+import com.iflytek.cloud.SpeechUtility;
+import com.iflytek.cloud.ui.RecognizerDialog;
+import com.iflytek.cloud.ui.RecognizerDialogListener;
+import java.util.UUID;
+import android.widget.Toast;
+import java.util.ArrayList;
+import android.os.Handler;
+import android.os.Message;
+import java.io.InputStream;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    /**
+     * Called when the activity is first created.
+     */
+    TextView statusLabel;
+    Button btnConnect, btnSend, btnQuit;
+    private KqwSpeechCompound mKqwSpeechCompound;
+    EditText etReceived, etSend;
+    private String text = "请注意，前方有障碍物";
+    private TextView text1;//接收python数据的text
+    float i = 50;
+    private static final int TCP_SERVER_PORT = 9996;
+    //device var
+    private BluetoothAdapter mBluetoothAdapter = null;
+
+
+    private BluetoothSocket btSocket = null;
+
+    private OutputStream outStream = null;
+
+    private InputStream inStream = null;
+
+    //这条是蓝牙串口通用的UUID，不要更改
+    private static final UUID MY_UUID =
+            UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
+
+    private static String address = "80:DA:B2:72:D5:D6"; // <==要连接的目标蓝牙设备MAC地址
+
+
+    private ReceiveThread rThread = null;  //数据接收线程
+
+
+    //接收到的字符串
+    String ReceiveData = "";
+
+    MyHandler handler;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        new Thread(networkTask).start();//创建一个新的socket线程
+
+        mKqwSpeechCompound = new KqwSpeechCompound(this);
+        //首先调用初始化函数
+        Init();
+        InitBluetooth();
+        handler = new MyHandler();
+
+        btnConnect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //判断蓝牙是否打开
+                if (!mBluetoothAdapter.isEnabled()) {
+                    mBluetoothAdapter.enable();
+                }
+                mBluetoothAdapter.startDiscovery();
+
+
+                //创建连接
+                new ConnectTask().execute(address);
+
+            }
+        });
+
+
+        btnQuit.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+
+                if (btSocket != null) {
+                    try {
+                        btSocket.close();
+                        btSocket = null;
+                        if (rThread != null) {
+                            rThread.join();
+                        }
+                        statusLabel.setText("当前连接已断开");
+//						etReceived.setText("");
+                    } catch (IOException e) {
+
+                        e.printStackTrace();
+                    } catch (InterruptedException e) {
+
+                        e.printStackTrace();
+                    }
+                }
+
+
+            }
+        });
+
+        btnSend.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                new SendInfoTask().execute(etSend.getText().toString());
+
+				/*
+					      byte[] msgBuffer = etSend.getText().toString().getBytes();
+
+					      System.out.println("to be send:"+etSend.getText().toString());
+
+					      try {
+
+					      System.out.println("size be send:"+msgBuffer.length);
+				          outStream.write(msgBuffer);
+
+					       } catch (IOException e) {
+					           Log.e("error", "ON RESUME: Exception during write.", e);
+					           //return "发送失败";
+				       }
+					      System.out.println("发送成功");
+					      */
+
+            }
+        });
+
+    }
+
+    //创建第二个线程，与python程序交互
+
+    Runnable networkTask = new Runnable() {
+        @Override
+        public void run() {
+            try {
+                Socket s = new Socket("10.20.87.199", TCP_SERVER_PORT);//注意host改成你服务器的hostname或IP地址
+                BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
+                BufferedWriter out = new BufferedWriter(new OutputStreamWriter(s.getOutputStream()));
+                //send output msg
+                String outMsg = "TCP connecting to " + TCP_SERVER_PORT + System.getProperty("line.separator");
+                out.write(outMsg);//发送数据
+                out.flush();
+                Log.i("TcpClient", "sent: " + outMsg);
+                //accept server response
+                String inMsg = in.readLine() + System.getProperty("line.separator");//得到服务器返回的数据
+
+                if(inMsg.length()>0)
+                {
+                    mKqwSpeechCompound.speaking("您已偏离盲道".trim());
+                }
+                Log.i("TcpClient", "received: " + inMsg);
+                //close connection
+                s.close();
+            } catch (UnknownHostException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    };
+
+
+
+
+        public void Init()
+    {
+        statusLabel=(TextView)this.findViewById(R.id.textView1);
+        btnConnect=(Button)this.findViewById(R.id.button1);
+        btnSend=this.findViewById(R.id.button2);
+        btnQuit=this.findViewById(R.id.button3);
+        etSend=this.findViewById(R.id.editText1);
+        etReceived=this.findViewById(R.id.editText2);
+    }
+
+    public void InitBluetooth()
+    {
+        //得到一个蓝牙适配器
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if(mBluetoothAdapter == null)
+        {
+            Toast.makeText(this, "你的手机不支持蓝牙", Toast.LENGTH_LONG).show();
+            finish();
+            return;
+        }
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+
+    //连接蓝牙设备的异步任务
+    class ConnectTask extends AsyncTask<String,String,String>
+    {
+
+
+        @Override
+        protected String doInBackground(String... params) {
+            // TODO Auto-generated method stub
+            BluetoothDevice device = mBluetoothAdapter.getRemoteDevice(params[0]);
+
+            try {
+
+                btSocket = device.createRfcommSocketToServiceRecord(MY_UUID);
+
+
+                btSocket.connect();
+
+                Log.e("error", "ON RESUME: BT connection established, data transfer link open.");
+
+            } catch (IOException e) {
+
+                try {
+                    btSocket.close();
+                    return "Socket 创建失败";
+
+                } catch (IOException e2) {
+
+                    Log .e("error","ON RESUME: Unable to close socket during connection failure", e2);
+                    return "Socket 关闭失败";
+                }
+
+            }
+            //取消搜索
+            mBluetoothAdapter.cancelDiscovery();
+
+            try {
+                outStream = btSocket.getOutputStream();
+
+                // inStream = btSocket.getInputStream();
+
+            } catch (IOException e) {
+                Log.e("error", "ON RESUME: Output stream creation failed.", e);
+                return "Socket 流创建失败";
+            }
+
+
+            return "蓝牙连接正常,Socket 创建成功";
+        }
+
+        @Override    //这个方法是在主线程中运行的，所以可以更新界面
+        protected void onPostExecute(String result) {
+            // TODO Auto-generated method stub
+
+            //连接成功则启动监听
+            rThread=new ReceiveThread();
+
+            rThread.start();
+
+            statusLabel.setText(result);
+
+            super.onPostExecute(result);
+        }
+
+
+
+    }
+
+    //发送数据到蓝牙设备的异步任务
+    class SendInfoTask extends AsyncTask<String,String,String>
+    {
+
+        @Override
+        protected void onPostExecute(String result) {
+            // TODO Auto-generated method stub
+            super.onPostExecute(result);
+
+            statusLabel.setText(result);
+
+            //将发送框清空
+            etSend.setText("");
+        }
+
+        @Override
+        protected String doInBackground(String... arg0) {
+            // TODO Auto-generated method stub
+
+            if(btSocket==null)
+            {
+                return "还没有创建连接";
+            }
+
+            if(arg0[0].length()>0)//不是空白串
+            {
+                //String target=arg0[0];
+
+                byte[] msgBuffer = arg0[0].getBytes();
+
+                try {
+                    //  将msgBuffer中的数据写到outStream对象中
+                    outStream.write(msgBuffer);
+
+                } catch (IOException e) {
+                    Log.e("error", "ON RESUME: Exception during write.", e);
+                    return "发送失败";
+                }
+
+            }
+
+            return "发送成功";
+        }
+
+    }
+
+
+    //从蓝牙接收信息的线程
+    class ReceiveThread extends Thread
+    {
+
+        String buffer="";
+
+        @Override
+        public void run() {
+
+            while(btSocket!=null )
+            {
+                //定义一个存储空间buff
+                byte[] buff=new byte[1024];
+                try {
+                    inStream = btSocket.getInputStream();
+                    System.out.println("waitting for instream");
+                    inStream.read(buff); //读取数据存储在buff数组中
+//                        System.out.println("buff receive :"+buff.length);
+
+                    // ReceiveData = new String(buff,0,inStream.available());
+                    //ReceiveData = new String(buff,"ASCII");
+
+                    processBuffer(buff,1024);
+
+                    //System.out.println("receive content:"+ReceiveData);
+                } catch (IOException e) {
+
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        private void processBuffer(byte[] buff,int size)
+        {
+            int length=0;
+            for(int i=0;i<size;i++)
+            {
+//				if(buff[i]>'\0')
+                if(buff[i]>'\0')
+                {
+                    length++;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+//			System.out.println("receive fragment size:"+length);
+
+            byte[] newbuff=new byte[length];  //newbuff字节数组，用于存放真正接收到的数据
+
+            for(int j=0;j<length;j++)
+            {
+                newbuff[j]=buff[j];
+            }
+
+            ReceiveData=ReceiveData+new String(newbuff);
+
+          if(buff.length>0)
+            {
+
+                mKqwSpeechCompound.speaking(text.trim());
+
+            }
+//			ReceiveData=new String(newbuff);
+            Log.e("Data",ReceiveData);
+//			System.out.println("result :"+ReceiveData);
+
+//			Message msg=new Message();  //by ywq
+            Message msg=Message.obtain();
+            msg.what=1;
+            handler.sendMessage(msg);  //发送消息:系统会自动调用handleMessage( )方法来处理消息
+
+        }
+
+    }
+
+
+
+    //更新界面的Handler类
+    class MyHandler extends Handler{
+
+        @Override
+        public void handleMessage(Message msg) {
+
+            switch(msg.what){
+                case 1:
+                   etReceived.setText(ReceiveData);
+
+                        break;
+                    }
+
+            }
+
+
+    }
+    /*public class SocketClient {
+
+        public  void main(String args[])throws Exception {
+
+            try {
+                Socket socket = new Socket("localhost",8001);
+
+                //获取输出流，向服务器端发送信息
+                OutputStream os=socket.getOutputStream();//字节输出流
+                PrintWriter pw=new PrintWriter(os);//将输出流包装为打印流
+                pw.write("我是Java服务器");
+                pw.flush();
+                socket.shutdownOutput();//关闭输出流
+
+                InputStream is=socket.getInputStream();
+                BufferedReader in = new BufferedReader(new InputStreamReader(is));
+                String info=null;
+                while((info=in.readLine())!=null){
+                    System.out.println("我是客户端，Python服务器说："+info);
+                }
+                is.close();
+                in.close();
+                socket.close();
+            } catch (UnknownHostException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }*/
+
+    @Override
+    protected void onDestroy() {
+        // TODO Auto-generated method stub
+        super.onDestroy();
+
+        try {
+            if(rThread!=null)
+            {
+
+                btSocket.close();
+                btSocket=null;
+
+                rThread.join();
+            }
+
+            this.finish();
+
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+    }
+
+}
---
+++ TTSonline/app/src/main/java/com/example/acer/ttsonline/MyReceiver.java
@@ -0,0 +1,70 @@
+package com.example.acer.ttsonline;
+
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+import com.example.acer.ttsonline.ClsUtils;
+
+public class MyReceiver extends BroadcastReceiver {
+    String pin = "1234";  //此处为你要连接的蓝牙设备的初始密钥，一般为1234或0000
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // TODO: This method is called when the BroadcastReceiver is receiving
+        // an Intent broadcast.
+       // throw new UnsupportedOperationException("Not yet implemented");
+        String action = intent.getAction(); //得到action
+        Log.e("action1=", action);
+        BluetoothDevice btDevice=null;  //创建一个蓝牙device对象
+        // 从Intent中获取设备对象
+        btDevice = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+
+        if(BluetoothDevice.ACTION_FOUND.equals(action)){  //发现设备
+            Log.e("发现设备:", "["+btDevice.getName()+"]"+":"+btDevice.getAddress());
+
+            if(btDevice.getName().contains("FBT06"))//HC-05设备如果有多个，第一个搜到的那个会被尝试。
+            {
+                if (btDevice.getBondState() == BluetoothDevice.BOND_NONE) {
+
+                    Log.e("ywq", "attemp to bond:"+"["+btDevice.getName()+"]");
+                    try {
+                        //通过工具类ClsUtils,调用createBond方法
+                        ClsUtils.createBond(btDevice.getClass(), btDevice);
+                    } catch (Exception e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+                }
+            }else
+                Log.e("error", "Is faild");
+        }else if(action.equals("android.bluetooth.device.action.PAIRING_REQUEST")) //再次得到的action，会等于PAIRING_REQUEST
+        {
+            Log.e("action2=", action);
+            if(btDevice.getName().contains("FBT06"))
+            {
+                Log.e("here", "OKOKOK");
+
+                try {
+
+                    //1.确认配对
+                    ClsUtils.setPairingConfirmation(btDevice.getClass(), btDevice, true);
+                    //2.终止有序广播
+                    Log.i("order...", "isOrderedBroadcast:"+isOrderedBroadcast()+",isInitialStickyBroadcast:"+isInitialStickyBroadcast());
+                    abortBroadcast();//如果没有将广播终止，则会出现一个一闪而过的配对框。
+                    //3.调用setPin方法进行配对...
+                    boolean ret = ClsUtils.setPin(btDevice.getClass(), btDevice, pin);
+
+                } catch (Exception e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }else
+                Log.e("提示信息", "这个设备不是目标蓝牙设备");
+
+        }
+    }
+}
+
+
